62f588857de37530a51af03048675f9ad33dee98
nguwenezapchristian <nguwenezacc982@gmail.com>
2023-07-17 22:14:34 +0200

    started to write the game function which calls roundPlay 5 times

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function getComputerChoice() {
-    let computerChoice = ["Rock", "Paper", "Scissors"];
+let computerChoice = ["Rock", "Paper", "Scissors"];
+function getComputerChoice(computerChoice) {
     let randomChoice = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[randomChoice];
 }
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "Rock" && playerSelection === "Rock") {
+    let playerSelection1 = playerSelection.toLowerCase();
+    let computerSelection1 = computerSelection.toLowerCase();
+    if (computerSelection1 === "rock" && playerSelection1 === "rock") {
         return "Play again! your choices were the same";
-    } else if(computerSelection === "Paper" && playerSelection === "Rock") {
+    } else if(computerSelection1 === "paper" && playerSelection1 === "rock") {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+    } else if (computerSelection1 === "scissors" && playerSelection1 === "rock") {
         return `You Won! ${playerSelection} beats ${computerSelection}`;
     }
 }
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
+const playerSelection = prompt("What's your choice?");
+const computerSelection = getComputerChoice(computerChoice);
 playRound(playerSelection, computerSelection);
+function game() {
+    
+}
