ea60a3cc0da342905c10f3975ec633dacd28b078
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-04 18:40:18 +0200

    made changes

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 let computerChoice = ["Rock", "Paper", "Scissors"];
 let score = 0;
+
 function getComputerChoice(computerChoice) {
     let randomChoice = Math.floor(Math.random() * computerChoice.length);
     return computerChoice[randomChoice];
 }
+
 function playRound(playerSelection, computerSelection) {
     let playerSelection1 = playerSelection.toLowerCase();
     let computerSelection1 = computerSelection.toLowerCase();
+    
     if (computerSelection1 === "rock" && playerSelection1 === "rock") {
-        return "Play again! your choices were the same";
-    } else if(computerSelection1 === "paper" && playerSelection1 === "rock") {
+        return "Play again! Your choices were the same";
+    } else if (computerSelection1 === "paper" && playerSelection1 === "rock") {
+        score--;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (computerSelection1 === "scissors" && playerSelection1 === "rock") {
-        score += 1;
+        score++;
         return `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return "Invalid choice! Please choose either Rock, Paper, or Scissors.";
     }
 }
-const playerSelection = prompt("What's your choice?");
-const computerSelection = getComputerChoice(computerChoice);
 
 function game() {
-   for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
-   }
-   if (score > 2) {
-    console.log(`You Won! with ${score} score`);
-   } else {
-    console.log(`You Lose! with ${score} score`)
-   }
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("What's your choice?").trim();
+        const computerSelection = getComputerChoice(computerChoice);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    
+    if (score > 0) {
+        console.log(`You Won! with a score of ${score}`);
+    } else if (score < 0) {
+        console.log(`You Lose! with a score of ${score}`);
+    } else {
+        console.log("It's a tie! No one wins.");
+    }
 }
+
+game();
