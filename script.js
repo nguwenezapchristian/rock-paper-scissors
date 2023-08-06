09a765d8ebb3c67dcffd622f8df4aa9cd651ac86
Nguweneza P Christian <nguwenezacc982@gmail.com>
2023-08-06 21:58:46 +0200

    updates

--- script.js
+++ script.js
@@ -1,42 +1,99 @@
-let computerChoice = ["Rock", "Paper", "Scissors"];
-let score = 0;
+let computerChoices = ["rock", "paper", "scissor"];
+let computerScore = 0;
+let playerScore = 0;
 
-function getComputerChoice(computerChoice) {
-    let randomChoice = Math.floor(Math.random() * computerChoice.length);
-    return computerChoice[randomChoice];
+function getComputerChoice(computerChoices) {
+    let randomChoice = Math.floor(Math.random() * computerChoices.length);
+    return computerChoices[randomChoice];
 }
+const paperBtn = document.querySelector("#paper-btn");
+const scissorBtn = document.querySelector("#scissor-btn");
+const rockBtn = document.querySelector("#rock-btn");
+const player = document.querySelector("#player-score");
+const pc = document.querySelector("#computer-score");
+const displayInfo = document.querySelector("#display-info");
+const displayWinnerOnScreen = document.querySelector("#display-winner");
 
-function playRound(playerSelection, computerSelection) {
-    let playerSelection1 = playerSelection.toLowerCase();
-    let computerSelection1 = computerSelection.toLowerCase();
-    
-    if (computerSelection1 === "rock" && playerSelection1 === "rock") {
-        return "Play again! Your choices were the same";
-    } else if (computerSelection1 === "paper" && playerSelection1 === "rock") {
-        score--;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (computerSelection1 === "scissors" && playerSelection1 === "rock") {
-        score++;
-        return `You Won! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return "Invalid choice! Please choose either Rock, Paper, or Scissors.";
+function displayWinner(winner) {
+    if (winner === "player") {
+        displayWinnerOnScreen.textContent = "Congratulations! You won the game!🦾";
+    } else if (winner === "computer") {
+        displayWinnerOnScreen.textContent = "Oops! Computer won the game!🙃";
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What's your choice?").trim();
-        const computerSelection = getComputerChoice(computerChoice);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    
-    if (score > 0) {
-        console.log(`You Won! with a score of ${score}`);
-    } else if (score < 0) {
-        console.log(`You Lose! with a score of ${score}`);
-    } else {
-        console.log("It's a tie! No one wins.");
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScores();
+}
+
+function updateScores() {
+    player.textContent = playerScore;
+    pc.textContent = computerScore;
+
+    if (playerScore === 5) {
+        displayWinner("player");
+        resetGame();
+    } else if (computerScore === 5) {
+        displayWinner("computer");
+        resetGame();
     }
 }
 
-game();
+paperBtn.addEventListener('click', () => {
+    switch(getComputerChoice(computerChoices)) {
+        case "rock":
+            playerScore++;
+            displayInfo.textContent = "😀 Paper beats Rock";
+            break;
+        case "paper":
+            displayInfo.textContent = "Play again! Your choices were the same";
+            break;
+        case "scissor":
+            computerScore++;
+            displayInfo.textContent = "😞 Scissor beats Paper";
+            break;
+        default:
+            console.log("Try Again");
+    }
+    updateScores();
+});
+
+scissorBtn.addEventListener('click', () => {
+    switch(getComputerChoice(computerChoices)) {
+        case "rock":
+            computerScore++;
+            displayInfo.textContent = "😞 Rock beats Scissor";
+            break;
+        case "paper":
+            playerScore++;
+            displayInfo.textContent = "😀 Paper beats Scissor";
+            break;
+        case "scissor":
+            displayInfo.textContent = "Play again! Your choices were the same";
+            break;
+        default:
+            console.log("Try Again");
+    }
+    updateScores();
+});
+rockBtn.addEventListener('click', () => {
+    switch(getComputerChoice(computerChoices)) {
+        case "rock":
+            displayInfo.textContent = "Play again! Your choices were the same";
+            break;
+        case "paper":
+            computerScore++;
+            displayInfo.textContent = "😞 Rock beats Scissor";
+            break;
+        case "scissor":
+            playerScore++;
+            displayInfo.textContent = "😀 Paper beats Rock";
+            break;
+        default:
+            console.log("Try Again");
+    }
+    updateScores();
+});
+
